32e1171020563cfc320d7dbdf53bdfe9c71d8d47
Joshua Sanders <ninjosh2@gmail.com>
2019-12-19 21:43:54 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,16 @@
-# Make your shoe class here!
+class Shoe 
+  def initialize(brand)
+    @brand = brand 
+  end 
+  
+  attr_reader :brand 
+  attr_accessor :color, :size, :material, :condition
+  
+  def cobble()
+    self.condition= "new" 
+    puts "Your shoe is good as new!" 
+  end 
+  
+end
+
+Shoe.new("Nike") 
